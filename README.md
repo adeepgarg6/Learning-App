f0359cf4886e1abf6855a3098b140e4bf7356ac1
adeepgarg6 <adeepgarg6@gmail.com>
2020-06-12 18:49:35 +0530

    Learning app

---
+++ README.md
@@ -0,0 +1,15 @@
+To Start
+ run node api/server.js
+ run npm start in another
+
+ deployed at https://awesome-mcclintock-b51659.netlify.app/
+
+ dates weren't given for all courses so sorting in form of dates is little abrupt
+
+ I have also provided the pagination
+
+ /api includes backend part
+
+ /public and /src contains frontend part
+
+ will work even if we provide partial provider, next session date, child subject
---
+++ src/repository.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000';
+//const BASE_URL = 'https://glacial-gorge-77575.herokuapp.com';
+
+export function getProducts(date,length,provider,page,next,child) {
+	console.log(date+' '+length+' '+provider+' '+page+' '+next+' '+child)
+	return axios.get(`${BASE_URL}/api/products?date=${date}&length=${length}&provider=${provider}&page=${page}&next=${next}&child=${child}`)
+		.then(response => response.data);
+}
+
